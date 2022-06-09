b6951823853837055c77cd5a77fe189f3a594c34
nikoteressi <nikoteressi@gmai.lcom>
2022-06-09 13:59:18 +0300

    docs: changed header.html, testPage.html       added and changed styles for result page in maim.css

--- src/main/java/com/example/dits/controllers/TestPageController.java
+++ src/main/java/com/example/dits/controllers/TestPageController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import java.util.*;
 
-@SuppressWarnings("unchecked")
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -23,25 +22,26 @@ public class TestPageController {
     private final TopicService topicService;
 
     @GetMapping("/goTest")
-    public String goTest(@RequestParam int testId, @RequestParam(value = "theme") String topicName,
-                         ModelMap model, HttpSession session) {
-        Test test = testService.getTestByTestId(testId);
-        List<Question> questionList = questionService.getQuestionsByTest(test);
+    public String goTest(@RequestParam int testId, @RequestParam(value = "theme") String topicName, ModelMap model, HttpSession session) {
+
+        List<Question> questionList = questionService.getQuestionsByTest_TestId(testId);
+        int quantityOfQuestions = questionList.size();
         int questionNumber = 0;
+        List<Answer> answers = answerService.getAnswersFromQuestion(questionList.get(questionNumber));
         int quantityOfRightAnswers = 0;
 
-        List<Answer> answers = answerService.getAnswersFromQuestion(questionList.get(questionNumber));
-        String questionDescription = questionList.get(questionNumber).getDescription();
+        int topicId = topicService.getTopicByName(topicName).getTopicId();
+        String testName = testService.getTestByTestId(testId).getName();
+        String questionDescription = questionService.getDescriptionFromQuestionList(questionList, questionNumber);
 
-        session.setAttribute("testName", test.getName());
+        session.setAttribute("testName", testName);
         session.setAttribute("topicName", topicName);
-        session.setAttribute("topicId", topicService.getTopicByName(topicName).getTopicId());
-        session.setAttribute("questionSize", questionList.size());
+        session.setAttribute("topicId", topicId);
+        session.setAttribute("questionSize", quantityOfQuestions);
         session.setAttribute("quantityOfRightAnswers", quantityOfRightAnswers);
         session.setAttribute("statistics", new ArrayList<Statistic>());
         session.setAttribute("questions", questionList);
         session.setAttribute("questionNumber", ++questionNumber);
-        session.setAttribute("currentQuestion", questionList.get(questionNumber));
 
         model.addAttribute("question", questionDescription);
         model.addAttribute("answers", answers);
@@ -51,22 +51,25 @@ public class TestPageController {
 
     @GetMapping("/nextTestPage")
     public String nextTestPage(@RequestParam(value = "answeredQuestion", required = false) List<Integer> answeredQuestion,
-                               ModelMap model, HttpSession session) {
-        Question currentQuestion = (Question) session.getAttribute("currentQuestion");
+                               ModelMap model,
+                               HttpSession session) {
+
+        List<Question> questionList = (List<Question>) session.getAttribute("questions");
         int questionNumber = (int) session.getAttribute("questionNumber");
+        int quantityOfRightAnswers = (int) session.getAttribute("quantityOfRightAnswers");
         User user = (User) session.getAttribute("user");
-        boolean isCorrect = answerService.isRightAnswer(answeredQuestion, currentQuestion);
+        boolean isCorrect = answerService.isRightAnswer(answeredQuestion, questionList.get(questionNumber - 1));
 
-        List<Answer> answers = currentQuestion.getAnswers();
-
-        String questionDescription = currentQuestion.getDescription();
+        List<Answer> answers = answerService.getAnswersFromQuestion(questionList.get(questionNumber));
+        String questionDescription = questionService.getDescriptionFromQuestionList(questionList, questionNumber);
 
         List<Statistic> statisticList = (List<Statistic>) session.getAttribute("statistics");
         statisticList.add(Statistic.builder()
-                .question(currentQuestion)
+                .question(questionList.get(questionNumber - 1))
                 .user(user)
                 .correct(isCorrect).build());
-
+        if (isCorrect) ++quantityOfRightAnswers;
+        session.setAttribute("quantityOfRightAnswers", quantityOfRightAnswers);
         session.setAttribute("statistics", statisticList);
         session.setAttribute("questionNumber", ++questionNumber);
         model.addAttribute("question", questionDescription);
@@ -108,7 +111,7 @@ public class TestPageController {
     }
 
     private void checkIfResultPage(List<Question> questions, int questionNumber, boolean isCorrect, User user, List<Statistic> statisticList) {
-        if (!isResultPage(questionNumber, statisticList)) {
+        if (!isResultPage(questionNumber, statisticList.size())) {
             statisticList.add(Statistic.builder()
                     .question(questions.get(questionNumber - 1))
                     .user(user)
@@ -116,8 +119,8 @@ public class TestPageController {
         }
     }
 
-    private boolean isResultPage(int questionNumber, List<Statistic> statisticList) {
-        return statisticList.size() >= questionNumber;
+    private boolean isResultPage(int questionNumber, int statisticSize) {
+        return statisticSize >= questionNumber;
     }
 
 

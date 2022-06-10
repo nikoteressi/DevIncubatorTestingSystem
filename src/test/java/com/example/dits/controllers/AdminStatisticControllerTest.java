e33f667de9c902a384f4316edd28519c2af53a4a
nikoteressi <nikoteressi@gmai.lcom>
2022-06-10 22:08:13 +0300

    docs: added test for uri /getTestsStatistic       refactored method getTestStatistics(Topic topic) in StatisticServiceImpl       refactored AdminStatisticController

--- src/test/java/com/example/dits/controllers/AdminStatisticControllerTest.java
+++ src/test/java/com/example/dits/controllers/AdminStatisticControllerTest.java
@@ -1,25 +1,35 @@
 package com.example.dits.controllers;
 
+import com.example.dits.service.StatisticService;
+import com.example.dits.service.TestService;
+import com.example.dits.service.TopicService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
 
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(username = "admin", roles = "ADMIN")
 public class AdminStatisticControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
-    @WithMockUser(username = "admin",
-            authorities = {"ROLE_ADMIN"})
 
     @Test
     public void getAdminStatistic() throws Exception {
@@ -29,31 +39,36 @@ public class AdminStatisticControllerTest {
 
     @Test
     public void getUserStatistic() throws Exception {
-        mockMvc.perform(get("/admin/user-statistic")
-                        .with(user("admin").roles("ADMIN")))
+        mockMvc.perform(get("/admin/user-statistic"))
+                .andExpect(status().isOk());
+    }
+
+    @Test()
+    public void getTestStatisticByTopicId() throws Exception {
+        mockMvc.perform(get("/admin/getTestsStatistic")
+                        .param("id", String.valueOf(anyInt())))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void getUserStatisticsById() throws Exception {
         mockMvc.perform(get("/admin/get-users-statistic")
-                        .param("userId", String.valueOf(anyInt()))
-                        .with(user("admin").roles("ADMIN")))
+                        .param("userId", String.valueOf(anyInt())))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void removeStatisticById() throws Exception {
         mockMvc.perform(get("/admin/adminStatistic/removeStatistic/byId")
-                        .param("id", String.valueOf(anyInt()))
-                        .with(user("admin").roles("ADMIN")))
+                        .param("id", String.valueOf(anyInt())))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void removeAllStatistics() throws Exception {
-        mockMvc.perform(get("/admin/adminStatistic/removeStatistic/all")
-                        .with(user("admin").roles("ADMIN")))
+        mockMvc.perform(get("/admin/adminStatistic/removeStatistic/all"))
                 .andExpect(redirectedUrl("/admin/adminStatistic"));
     }
 
